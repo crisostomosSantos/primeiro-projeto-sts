4ee4632f18671ad46e0d11c893d1c9f34c49a708
crisostomosSantos <criscgweb@gmail.com>
2019-10-01 16:01:33 -0300

    Commit Inicial

---
+++ src/main/java/com/devprojts/CorrespondenteCaixa/api/CorrespondenteCaixaApplication.java
@@ -0,0 +1,13 @@
+package com.devprojts.CorrespondenteCaixa.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorrespondenteCaixaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorrespondenteCaixaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devprojts/CorrespondenteCaixa/api/CorrespondenteCaixaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.devprojts.CorrespondenteCaixa.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CorrespondenteCaixaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
